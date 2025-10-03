25d96d3c7fb88d24db9437059f1b563d1566fa00
sandeepsharmadeveloper5373 <sandeepsharmadeveloper5373@gmail.com>
2025-10-03 06:07:22 +0530

    Update project

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { registerRootComponent } from 'expo';
 import { ExpoRoot } from 'expo-router';
 
 // Must be exported or React Native will error
-export function App() {
+export default function App() {
   const ctx = require.context('./app');
   return <ExpoRoot context={ctx} />;
 }
